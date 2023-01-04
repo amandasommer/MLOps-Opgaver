57c77ee9cf07ee0e6e8b7008f095684712ef3867
Amanda Sommer <amanda.m.sommer@hotmail.com>
2023-01-04 15:30:05 +0100

    hov

--- data.py
+++ data.py
@@ -4,7 +4,8 @@ import numpy as np
 
 def mnist():
     # exchange with the corrupted mnist dataset
-    train = np.load('test.npz') 
+    y=1
+    train = np.load('test.npz') #Hov
     test = np.load('test.npz') 
     return train, test
 
